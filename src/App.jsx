1058a221c72dad7058de7abefb4a2bd36b74a585
Farhan Falak Sher <farhanfalaksher533@gmail.com>
2025-11-10 16:26:07 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 
 import "./App.css";
 import Navbar from "./pages/Navbar";
@@ -23,18 +24,17 @@ function App() {
   }, []);
 
   return (
-    
-      
+    <>
+      <Navbar />
       <Routes>
-        <Navbar />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/author" element={<Author />} />
-        <Route path="/theme" element={<Theme />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/order" element={<GetTheBookPreOrder />} />
+        <Route path ="/theme" element={<Theme/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/order" element={<GetTheBookPreOrder/>}/>
       </Routes>
-    
+    </>
   );
 }
 
