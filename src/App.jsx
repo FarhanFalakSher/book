1d4fa33ad0109372babbe8883cf78f25f7b20d12
Farhan Falak Sher <farhanfalaksher533@gmail.com>
2025-11-10 18:02:07 +0500

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
 import "./App.css";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
@@ -30,9 +29,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/author" element={<Author />} />
-        <Route path ="/theme" element={<Theme/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/order" element={<GetTheBookPreOrder/>}/>
+        <Route path="/theme" element={<Theme />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/order" element={<GetTheBookPreOrder />} />
       </Routes>
     </>
   );
