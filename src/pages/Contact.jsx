33fa9d7554c9f88441fc5eb2de99ffaa0c1358e8
FarhanFalakSher <farhanfalaksher533@gmail.com>
2025-11-12 11:47:04 +0500

    set path

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ContactHeroSection from '../components/contact/ContactHeroSection'
 import MainSection from '../components/contact/MainSection'
-// import ContactHeroSection from '../components/contact/ContactHeroSection'
-// import ContactMainSection from '../components/contact/ContactMainSection'
-// import Footer from './Footer'
+
 
 const Contact = () => {
   return (
     <div>
-        {/* <ContactHeroSection/>
-        <ContactMainSection/> */}
-        {/* <Footer/> */}
+       
         <ContactHeroSection/>
-        <MainSection/>
+       <MainSection/>
     </div>
   )
 }
